81bc45031c80b0620ec57393884abe8780f9a6f6
VR <vladrymarchuk@gmail.com>
2024-12-28 02:09:10 +0200

    add views, urls for Actor

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,11 +3,17 @@ from django.urls import path
 from cinema.views import (
     GenreList,
     GenreDetail,
+    ActorList,
+    ActorDetail,
 )
 
 urlpatterns = [
     path("genres/", GenreList.as_view(), name="genres-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genres-detail"),
+
+
+    path("actors/", ActorList.as_view(), name="actors-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actors-detail"),
 
 
 ]
